aa3f8fd412f9da945b2eb37d5d77e2142c4dd934
lya0210 <lya8690@naver.com>
2023-08-08 16:39:34 +0900

    [FEAT] 홈페이지/기타페이지 기초 세팅, 라우터 작업

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Home from './screen/Home';
+import Application from './screen/Application';
+import ApplicationHistory from './screen/ApplicationHistory';
+import Admin from './screen/Admin';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/application' element={<Application />} />
+        <Route
+          path='/applicationhistory/:email'
+          element={<ApplicationHistory />}
+        />
+        <Route path='/admin' element={<Admin />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
